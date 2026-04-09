1d4cf84f81dd9461d43d2c9e933902b4e4bfeeea
Benjamín-Serrano <alumno26.serrano.benjamin@ipm.edu.ar>
2026-04-09 15:36:13 -0300

    Creamos el proyecto SpringBoot

---
+++ OrtizSerranoTP3/src/main/java/com/DecanatoOrtizSerrano/OrtizSerranoTP3/OrtizSerranoTp3Application.java
@@ -0,0 +1,13 @@
+package com.DecanatoOrtizSerrano.OrtizSerranoTP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrtizSerranoTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrtizSerranoTp3Application.class, args);
+	}
+
+}
